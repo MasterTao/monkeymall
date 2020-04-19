25987c3ade4eaf4dfc2302cd324a6fbcfa09582b
txu1 <txu1@i9i8.com>
2020-04-19 17:11:49 +0800

    项目构建初始提交

---
+++ mall-coupon/src/test/java/com/monkey/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkey.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
